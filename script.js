fa0f771f394bba88601915e565a8cde28ae8d2c7
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-02-25 18:12:38 -0500

    added some comments to explain code and added ordinal dates

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
+  //saves the text in a div
   $('.saveBtn').click(function(){
     var currentDiv = $(this).parent().attr('id');
     var savedText = $('#' + currentDiv).children('textarea').val();
     localStorage.setItem(currentDiv, savedText); 
   });
 
+  // changes the color of the divs
   var currentHour = dayjs().format('H');
   for (i = 9; i < 18; i++) {
     if (i < currentHour) {
@@ -16,10 +18,12 @@ $(document).ready(function () {
     }
   }
   
+  // places the text into the divs from local storage
   for (i = 9; i < 18; i++) {
     var quickGet = localStorage.getItem(i);
     $("#" + i).children('textarea').val(quickGet);
   }
 
-  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+  //sets the day
+  $('#currentDay').text(dayjs().format('dddd, MMMM Do, YYYY'));
 });
